fdd5bb25df007d2d9a1d905ef36549f8b4823744
Cannabis2013 <havnetrold2002@yhaoo.dk>
2019-11-21 22:20:17 +0100

    Implementing 'radio_send' function. Look at comments.

--- radio.c
+++ radio.c
@@ -3,24 +3,25 @@
 int radio_init(int addr)
 {
     struct in_addr adrs;
-    adrs.s_addr = addr;
-    char *data = inet_ntoa(adrs);
-    if(inet_aton(data,&adrs) == 0)
+    adrs.s_addr = (uint) addr;
+    if(inet_aton(LOCALHOST,&adrs) == 0)
     {
-        // TODO: Implement some validy confirmation
+        printf("%s","Not a valid IPv4 adress! Did you enter your household adress you idiot?");
+        return -1;
     }
 
     /* Verify that the port is within the desired range
      * In this case we don't want to use ports below 100 or above 60000.
      */
 
-    if(addr < 100 && addr > 60000)
+    if(addr < 100 || addr > 60000)
     {
-        // TODO: Implement some validy confirmation
+        printf("%s","Not a valid port. Did you enter the addres of your butt you dum liberal?");
+        return -1;
     }
 
     ClientService.sin_family = AF_INET;
-    ClientService.sin_port = htons(3377);
+    ClientService.sin_port = htons(LOCALADRESS);
     ClientService.sin_addr.s_addr =(uint) adrs.s_addr;
 
     mySocket = socket(AF_INET,SOCK_STREAM,IPPROTO_UDP);
@@ -31,6 +32,28 @@ int radio_init(int addr)
         return -1;
     }
 
-    //int connection = connect(mySocket,(struct sockaddr *)&ClientService,sizeof (ClientService));
     return 0;
+}
+
+int radio_send(int dst, char *data, int len)
+{
+    if(inet_aton(&dst,NULL) == 0)
+    {
+        // TODO: Implement some error handling or specifikation
+        return -1;
+    }
+
+    int connection = connect(mySocket,(struct sockaddr *)&ClientService,sizeof (ClientService));
+
+    // Check if connection is established and handle the error if necessary
+    if(connection < 0)
+    {
+        // TODO: Implement error handling
+        // NOTE: The connection variable is -1 if socket failed to establish connection to the destination
+        return -1;
+    }
+    if(send(mySocket,NULL,0,0) < 0)
+    {
+
+    }
 }
--- radio.h
+++ radio.h
@@ -1,3 +1,22 @@
+/*
+ *      Notes for the group regarding the use of the sys/socket library.
+ *
+ *      For information about functions and their return values please visit the following:
+ *
+ *          Url: https://pubs.opengroup.org/onlinepubs/7908799/xns/connect.html
+ *
+ *      The link contains some usefull description of functions and definitions that might prove usefull
+ *      in relation to usage and further clarification.
+ *
+ *      My issues (Martin Hansen) can be outlined as follows:
+ *          - Do we need to implement the loop here?
+ *              - If yes; should we make use of the 'pthread' library (threading)?
+ *          - Should we just stick to a local domain driven interface?
+ *              (We use ports as localadress as specified in the document and ignore internet functionality)
+ *          - Shouldn't we force Peter to make the whole assignment blindfolded? :D
+ */
+
+
 #ifndef RADIO_H
 #define RADIO_H
 
@@ -12,7 +31,8 @@
 
 //#include "errors.h"
 #define FRAME_PAYLOAD_SIZE 72
-#define RECIEVER_ADRS "127.0.0.1"
+#define LOCALHOST "127.0.0.1"
+#define LOCALADRESS 55000
 
 extern struct sockaddr_in ClientService;
 extern int mySocket;
